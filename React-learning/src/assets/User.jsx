ba8171ace1b6db67316f1e91b4e75f8c8e8345ba
Dhinaseelan <dhinaseeland@gmail.com>
2026-02-07 21:58:02 +0530

    feat:adding features like if you purchesed or not and showing discout when you clicking buy now button using useState hook

--- React-learning/src/assets/User.jsx
+++ React-learning/src/assets/User.jsx
@@ -1,14 +1,30 @@
 import PropTypes from 'prop-types';
 import './user.css';
+import { useState } from 'react';
 
 function User({ img, price, title, display ,rating}) {
+  const [buy,setbuy]=useState(false);
+  const [dis,setdis]=useState()
+  const discountap=(offer)=>{alert(price+" purched "+offer+"% offer aplied are you sure to buy")
+    function recipt(){
+      setbuy(true);
+        setdis(title-(offer/100*title))
+        alert(`total bill: ${title - (offer / 100 * title)}`)
+        console.log("total bill: "(title-(offer/100*title)))
+        
+    }
+    recipt()
+    
+  }
   if (display==true) {
     return (
       <div className="box">
         <img src={img} alt="user" />
         <h3>{price}</h3>
         <h4> Rating:<span style={{color:"red"}}>{rating}</span></h4>
-        <h2>${title}</h2>
+        <h2>${dis?dis:title}</h2>
+        <button onClick={()=>discountap(20)}>Buy Now</button>
+        <p>{buy?"already purched":"Buy Now"}</p>
       </div>
     );
   } else {
